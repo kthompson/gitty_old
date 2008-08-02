9333d0412f900db643cc34fdb314ac57dafed1a9
Kevin Thompson <mrunleaded@gmail.com>
2008-08-01 21:50:26 -0700

    Completed CoreConfig Added Reference to SharpZipLib Included SharpZipLib sources

--- Gitty/Lib/CoreConfig.cs
+++ Gitty/Lib/CoreConfig.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ICSharpCode.SharpZipLib.Zip.Compression;
 
 namespace Gitty.Lib
 {
+    [Complete]
     public class CoreConfig
     {
+        public class Constants
+        {
+            public static readonly int DefaultCompression = Deflater.DEFAULT_COMPRESSION;
+        }
+
+        public int PackIndexVersion { get; private set; }
+        public int Compression { get; private set; }
+
         public CoreConfig(RepositoryConfig repoConfig)
         {
-            throw new NotImplementedException();
+            this.Compression = repoConfig.GetInt("core", "compression", Constants.DefaultCompression);
+            this.PackIndexVersion = repoConfig.GetInt("pack", "indexversion", 0);
         }
     }
 }

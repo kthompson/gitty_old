09f940dbcc6d0371e6a7973c06be73c1e1058c04
Kevin Thompson <mrunleaded@gmail.com>
2008-08-03 01:39:45 -0700

    Completed AbstractIndexTreeVisitor, IndexTreeVisitor

---
+++ Gitty/Lib/AbstractIndexTreeVisitor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Gitty.Lib
+{
+    [Complete]
+    public class AbstractIndexTreeVisitor : IndexTreeVisitor
+    {
+        public virtual void FinishVisitTree(Tree tree, Tree auxTree, String curDir)
+        {
+            // Empty
+        }
+
+        public virtual void FinishVisitTree(Tree tree, int i, String curDir)
+        {
+            // Empty
+        }
+
+        public virtual void VisitEntry(TreeEntry treeEntry, GitIndex.Entry indexEntry, FileInfo file)
+        {
+            // Empty
+        }
+
+        public virtual void VisitEntry(TreeEntry treeEntry, TreeEntry auxEntry,
+                GitIndex.Entry indexEntry, FileInfo file)
+        {
+            // Empty
+        }
+    }
+}

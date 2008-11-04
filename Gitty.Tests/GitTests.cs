2305e2ea26cc6b9a83f294bee553df7001028dd2
Kevin Thompson <kevin.thompson@theautomaters.com>
2008-11-04 15:06:13 -0800

    Removed old tests and added some new

---
+++ Gitty.Tests/GitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.SyntaxHelpers;
+using System.IO;
+
+namespace Gitty.Tests
+{
+    [TestFixture]
+    public class GitTests
+    {
+        [Test]
+        public void InitTest()
+        {
+            string repoPath = Path.Combine(Path.GetTempPath(), "git" + new Random().Next(int.MaxValue).ToString());
+            Git git = Git.Init(new DirectoryInfo(repoPath));
+            Assert.That(repoPath, Is.EqualTo(git.Repository.Directory.FullName));
+        }
+    }
+}

3f59de9e15090d8900b5d5dc2e4911c62fa92539
Kevin Thompson <kevin.thompson@theautomaters.com>
2008-11-05 08:21:58 -0800

    restructuring

--- Gitty.Core/Lib/FileMode.cs
+++ Gitty.Core/Lib/FileMode.cs
@@ -6,6 +6,7 @@ using System.IO;
 
 namespace Gitty.Lib
 {
+    [Complete]
     public class FileMode
     {
 
--- Gitty/Git.cs
+++ Gitty/Git.cs
@@ -8,49 +8,21 @@ namespace Gitty
     public partial class Git
     {
 
-        public ICoreLib Core { get; private set; }
 
         #region constructors
-        public Git(ICoreLib corelib)
+        private Git()
         {
-            if (corelib == null)
-                throw new ArgumentNullException("corelib cannot be null.");
-
-            this.Core = corelib;
-        }
-
-        public Git(Type corelib)
-        {
-            if(corelib == null)
-                throw new ArgumentNullException("corelib cannot be null.");
-
-            if (!typeof(ICoreLib).IsAssignableFrom(corelib))
-                throw new ArgumentException(string.Format("{0} is not assignable as {1}.", corelib.ToString(), typeof(ICoreLib).ToString()));
-
-            if (corelib.IsAbstract)
-                throw new ArgumentException("CoreLib cannot be an abstract class.");
-
-            if(!corelib.IsClass)
-                throw new ArgumentException("CoreLib must be a class.");
-
-            try
-            {
-                this.Core = (ICoreLib)Activator.CreateInstance(corelib);
-            }
-            catch(Exception e)
-            {
-                throw new ArgumentException("An error occurred while creating the specified ICoreLib class.", e);
-            }
+            
         }
         #endregion
 
         #region public static methods
-        public static DirectoryInfo FindGitDirectory()
+        private static DirectoryInfo FindGitDirectory()
         {
             return FindGitDirectory(new DirectoryInfo("."));
         }
 
-        public static DirectoryInfo FindGitDirectory(DirectoryInfo path)
+        private static DirectoryInfo FindGitDirectory(DirectoryInfo path)
         {
             if (!path.Exists)
                 throw new FileNotFoundException();
@@ -59,15 +31,15 @@ namespace Gitty
             if (dirs.Length > 0)
                 return dirs[0];
             else if(path.Parent == null)
-                throw new FileNotFoundException();
+                return null;
             else
                 return FindGitDirectory(path.Parent);
         }
 
-        public static DirectoryInfo FindGitDirectory(FileInfo path)
+        private static DirectoryInfo FindGitDirectory(FileInfo path)
         {
             if (!path.Exists)
-                throw new FileNotFoundException();
+                return null;
 
             return FindGitDirectory(path.Directory);
         }

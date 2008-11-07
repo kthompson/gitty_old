fec4db23acb01b131f3cd557dad147093f13c275
Kevin Thompson <mrunleaded@gmail.com>
2008-11-07 12:31:44 -0800

    added new interfaces

---
+++ Gitty/IIndex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Gitty
+{
+    public interface IIndex : IPath
+    {
+        void Checkout(string branch);
+        bool Add(FileInfo file, string sha);
+    }
+}
---
+++ Gitty/IPath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Gitty
+{
+    public interface IPath
+    {
+        DirectoryInfo Directory { get; }
+        FileInfo File { get; }
+    }
+}
